49ee138599cbd0efbcd19d921fa9d530d2d5e010
Charlie Haley <charlie.haley@hotmail.com>
2025-03-26 20:36:21 +0000

    Update docusaurus.config.ts

--- web/docs/docusaurus.config.ts
+++ web/docs/docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   organizationName: "marmotdata",
   projectName: "marmot",
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
