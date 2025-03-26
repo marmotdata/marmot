a8079cf0c5156f5b4325ad541e539c31c9f4ca2a
Charlie Haley <charlie.haley@hotmail.com>
2025-03-26 20:42:56 +0000

    Update docusaurus.config.ts (#4)

--- web/docs/docusaurus.config.ts
+++ web/docs/docusaurus.config.ts
@@ -13,10 +13,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://marmotdata.github.io/marmot",
+  url: "https://marmotdata.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/marmot",
 
   // GitHub pages deployment config.
   organizationName: "marmotdata",
