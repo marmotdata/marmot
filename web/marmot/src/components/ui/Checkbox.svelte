5b6eaa1f5faf9be47ef2bfeb53e7c55ff3bc5006
mjanez <96422458+mjanez@users.noreply.github.com>
2026-09-24 17:04:15 +0200

    feat(ui): style checkboxes with shared terracotta chrome

---
+++ web/marmot/src/components/ui/Checkbox.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from 'svelte/elements';
+
+	type Props = Omit<HTMLInputAttributes, 'type' | 'class'> & {
+		checked?: boolean;
+		class?: string;
+	};
+
+	let { checked = $bindable(false), class: className = '', disabled, ...rest }: Props = $props();
+</script>
+
+<input type="checkbox" bind:checked {disabled} class={className} {...rest} />
